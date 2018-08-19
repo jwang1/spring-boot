ae81c4ff3979787d6c7dd1986e870037b8416519
jwang1 <sigkdd@yahoo.com>
2018-08-19 01:20:28 -0400

    1. Updated the Integration tests for PropertyServiceForJasyptStarter    which is working well now
    2. Need to set the Jasypt pwd from unit-test-config (jasypt.encryptor.password=... as JVM args)
    
    3. Now it shows PropertyServiceForJasyptStarter works as well;
       and then, it is better than the way in NotesApplicationConfig class
    
    4. The other thing is the application-integrationtest.properties
       can be improved, say, to include application.properties of the app?

--- src/test/java/com/iExpress/notes/notes/service/PropertyServiceForJasyptStarterIntegrationTest.java
+++ src/test/java/com/iExpress/notes/notes/service/PropertyServiceForJasyptStarterIntegrationTest.java
@@ -7,11 +7,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+/**
+ * Spring-Boot Integration tests for {@link PropertyServiceForJasyptStarter}
+ * @author jwang
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-public class PropertyServiceForJasyptStarterTest {
+@TestPropertySource(
+        locations = "classpath:application-integrationtest.properties"
+)
+public class PropertyServiceForJasyptStarterIntegrationTest {
     @Autowired
     private ApplicationContext ctx;
 
@@ -24,7 +32,7 @@ public class PropertyServiceForJasyptStarterTest {
 
     @Test
     public void whenDecryptedPwdNeeded_GetFromServer() {
-        System.setProperty("spring.datasource.password", "password");
+        // System.setProperty("jasypt.encryptor.password", "CANNOT-PROVIDER-HERE__otherwise_DB-PWD_exposed----can-try-from-run-config-...");
         System.out.println(svc.getProperty());
 
         Environment env = ctx.getBean(Environment.class);
